60dabb113a862d91796a9f5b1d47fbd4eddedafc
Rajat Soni <rajat@Rajats-MacBook-Pro.local>
2020-04-12 09:16:19 +0530

    fix typo in app constants

--- src/app/stories.service.ts
+++ src/app/stories.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BASE_URL } from './app.contants';
+import { BASE_URL } from './app.constants';
 
 @Injectable({
   providedIn: 'root'
